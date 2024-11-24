ce6e1bbfe6884514421dcda30b4981dbb0330c0e
parshas <shachar.parshani@gamil.com>
2024-11-24 12:59:46 +0200

    load data file

--- load_data.py
+++ load_data.py
@@ -3,25 +3,46 @@ import pymysql
 
 # Database connection details
 host = 'localhost'
-user = 'your_username'
-password = 'your_password'
-database = 'your_database'
+user = 'root'
+password = 'shachar100'
+database = 'food_recommandation'
 
-# CSV file path
-csv_file_path = 'path_to_your_file.csv'
+# Paths to your CSV files
+food_data_big_csv = 'path_to_first_file.csv'
+food_data_small_csv = 'path_to_second_file.csv'
 
 # Connect to the database
 connection = pymysql.connect(host=host, user=user, password=password, database=database)
+cursor = connection.cursor()
 
 try:
-    # Read the CSV file into a DataFrame
-    df = pd.read_csv(csv_file_path)
+    # Step 1: Load and preprocess CSV 1
+    df_big = pd.read_csv(food_data_big_csv)
 
-    # Insert data into the SQL table
-    df.to_sql('your_table_name', con=connection, if_exists='append', index=False)
-    print("Data loaded successfully.")
+    # Step 2: Load and preprocess CSV 2
+    df_small = pd.read_csv(food_data_small_csv)
+
+    # Step 4: Insert data row by row
+    insert_query = "INSERT INTO food (food_name, Caloric_Value_kcal, Protein_g, Dietary_Fiber_g, Cholesterol_mg, Sodium_g" \
+                   ", Water_g, Vitamin_A, Thiamin_mg, Folic_acid_mg, Vitamin_B12_mg, Riboflavin_mg, Niacin_mg, Pantothenic_acid_mg" \
+                   ", Vitamin_B6_mg, Vitamin_C_mg, Vitamin_D_mg, Vitamin_E_mg, Vitamin_K_mg, Calcium_mg, Copper_mg, Iron_mg" \
+                   ", Magnesium_mg, Manganese_mg, Phosphorus_mg, , Potassium_mg, Selenium_mg, Zinc_mg) VALUES (%s, %s)"
+    for index, row in df_big.iterrows():
+        cursor.execute(insert_query, (
+            row['name'], row['Energy'], row['Protein'], row['Fiber, total dietary'], row['Cholesterol'], row['Sodium, Na'], row['Water'],
+            row['Vitamin A, RAE'], row['Thiamin'], row['Folic acid'], row['Vitamin B-12'], row['Riboflavin'], row['Niacin'],
+            row['Pantothenic acid'], row['Vitamin B-12'], row['Vitamin C, total ascorbic acid'], row['Vitamin D (D2 + D3)'], row['Vitamin E'],
+            row['Vitamin K'], row['Calcium'], row['Copper'], row['Iron'], row['Magnesium'], row['Manganese'],
+            row['Phosphorus, P'], row['Potassium, K'], row['Selenium, Se'], row['Zinc, Zn']
+        ))
+
+    # Commit the changes
+    connection.commit()
+    print("Data inserted successfully.")
 except Exception as e:
     print("An error occurred:", e)
+    connection.rollback()  # Rollback in case of error
 finally:
-    # Close the database connection
+    # Close the cursor and connection
+    cursor.close()
     connection.close()
