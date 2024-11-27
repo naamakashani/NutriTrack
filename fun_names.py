e3b3396bda7b1b88bf36fc7f8d035bf4e1b49bc4
parshas <shachar.parshani@gamil.com>
2024-11-27 15:25:16 +0200

    food data

---
+++ fun_names.py
@@ -0,0 +1,29 @@
+def get_daily_gap(user_id, date):
+    # return the daily gap of the user in the given date
+    # the gap is dict of the deficencies and the excesses , the amount of the deficencies and the excesses
+    return 0;
+def insert_eaten(user_id, date, food_name, amount):
+    # insert the eaten food to the eaten_table
+    return 0;
+def insert_user(user_id, user_name, age, gender, subgroup, weight, height, activity_level):
+    # insert a new user to the user_table
+    return 0;
+def create_new_team(team_name):
+    # create a new team
+    return 0;
+def join_team(team_id, user_id):
+    # join a team
+    return 0;
+def recommand_food(defic_list):
+    # return the recommanded food by the list of deficencies
+    # by top 10
+    return 0;
+def statistics(user_id):
+    # return the statistics of the user
+    return 0;
+def trends(user_id):
+    # return the trends of the user
+    return 0;
+def comparison_team(team_id):
+    # return the comparison of the team
+    return 0;
--- load_data.py
+++ load_data.py
@@ -1,48 +1,142 @@
 import pandas as pd
 import pymysql
 
-# Database connection details
-host = 'localhost'
-user = 'root'
-password = 'shachar100'
-database = 'food_recommandation'
+def load_food_data_big():
+    # Load data from CSV file - food data big and insert into the database,
+    # delete rows with NULL food_name and duplicate rows
 
-# Paths to your CSV files
-food_data_big_csv = 'path_to_first_file.csv'
-food_data_small_csv = 'path_to_second_file.csv'
+    # Database connection details
+    host = 'localhost'
+    user = 'root'
+    password = 'shachar100'
+    database = 'food_recommandation'
 
-# Connect to the database
-connection = pymysql.connect(host=host, user=user, password=password, database=database)
-cursor = connection.cursor()
+    # Paths to your CSV files
+    food_data_big_csv = 'FoodDataBig.csv'
+    food_data_small_csv = 'FoodDataSmall.csv'
 
-try:
-    # Step 1: Load and preprocess CSV 1
-    df_big = pd.read_csv(food_data_big_csv)
+    # Connect to the database
+    connection = pymysql.connect(host=host, user=user, password=password, database=database)
+    cursor = connection.cursor()
 
-    # Step 2: Load and preprocess CSV 2
-    df_small = pd.read_csv(food_data_small_csv)
+    try:
+        # Step 1: Load and preprocess CSV 1
+        df_big = pd.read_csv(food_data_big_csv)
 
-    # Step 4: Insert data row by row
-    insert_query = "INSERT INTO food (food_name, Caloric_Value_kcal, Protein_g, Dietary_Fiber_g, Cholesterol_mg, Sodium_g" \
-                   ", Water_g, Vitamin_A, Thiamin_mg, Folic_acid_mg, Vitamin_B12_mg, Riboflavin_mg, Niacin_mg, Pantothenic_acid_mg" \
-                   ", Vitamin_B6_mg, Vitamin_C_mg, Vitamin_D_mg, Vitamin_E_mg, Vitamin_K_mg, Calcium_mg, Copper_mg, Iron_mg" \
-                   ", Magnesium_mg, Manganese_mg, Phosphorus_mg, , Potassium_mg, Selenium_mg, Zinc_mg) VALUES (%s, %s)"
-    for index, row in df_big.iterrows():
-        cursor.execute(insert_query, (
-            row['name'], row['Energy'], row['Protein'], row['Fiber, total dietary'], row['Cholesterol'], row['Sodium, Na'], row['Water'],
-            row['Vitamin A, RAE'], row['Thiamin'], row['Folic acid'], row['Vitamin B-12'], row['Riboflavin'], row['Niacin'],
-            row['Pantothenic acid'], row['Vitamin B-12'], row['Vitamin C, total ascorbic acid'], row['Vitamin D (D2 + D3)'], row['Vitamin E'],
-            row['Vitamin K'], row['Calcium'], row['Copper'], row['Iron'], row['Magnesium'], row['Manganese'],
-            row['Phosphorus, P'], row['Potassium, K'], row['Selenium, Se'], row['Zinc, Zn']
-        ))
+        # Step 2: Load and preprocess CSV 2
+        df_small = pd.read_csv(food_data_small_csv)
 
-    # Commit the changes
+        # Step 3: Handle missing values - Replace 'NaN' with None
+        df_big.loc[df_big['name'].isna(), 'name'] = None
+
+        # Step 4: Insert data row by row
+        insert_query = "INSERT IGNORE INTO food (food_name, Caloric_Value_kcal, Protein_g, Dietary_Fiber_g, Cholesterol_mg, Sodium_g" \
+                    ", Water_g, Vitamin_A_mg, Thiamin_mg, Folic_acid_mg, Vitamin_B12_mg, Riboflavin_mg, Niacin_mg, Pantothenic_acid_mg" \
+                    ", Vitamin_B6_mg, Vitamin_C_mg, Vitamin_D_mg, Vitamin_E_mg, Vitamin_K_mg, Calcium_mg, Copper_mg, Iron_mg" \
+                    ", Magnesium_mg, Manganese_mg, Phosphorus_mg, Potassium_mg, Selenium_mg, Zinc_mg) VALUES (%s, %s," \
+                    " %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        for index, row in df_big.iterrows():
+            cursor.execute(insert_query, (
+                row['name'], row['Energy'], row['Protein'], row['Fiber, total dietary'], row['Cholesterol'], row['Sodium, Na'], row['Water'],
+                row['Vitamin A, RAE'], row['Thiamin'], row['Folic acid'], row['Vitamin B-12'], row['Riboflavin'], row['Niacin'],
+                row['Pantothenic acid'], row['Vitamin B-12'], row['Vitamin C, total ascorbic acid'], row['Vitamin D (D2 + D3)'], row['Vitamin E'],
+                row['Vitamin K (phylloquinone)'], row['Calcium, Ca'], row['Copper, Cu'], row['Iron, Fe'], row['Magnesium, Mg'], row['Manganese, Mn'],
+                row['Phosphorus, P'], row['Potassium, K'], row['Selenium, Se'], row['Zinc, Zn']
+            ))
+
+        # Commit the changes
+        connection.commit()
+        print("Data inserted successfully.")
+        # Step 5: Delete rows with NULL in the 'food_name' column
+        delete_query = "DELETE FROM food WHERE food_name IS NULL;"
+        cursor.execute(delete_query)
+        connection.commit()
+        print("Rows with NULL food_name deleted successfully.")
+    except Exception as e:
+        print("An error occurred:", e)
+        connection.rollback()  # Rollback in case of error
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+def convert_scale():
+    # convert the scale of the food data big from g to the appropriate measure according to the column name in the table
+    # Database connection details
+    host = 'localhost'
+    user = 'root'
+    password = 'shachar100'
+    database = 'food_recommandation'
+
+    # Paths to your CSV files
+    food_data_big_csv = 'FoodDataBig.csv'
+    food_data_small_csv = 'FoodDataSmall.csv'
+
+    # Connect to the database
+    connection = pymysql.connect(host=host, user=user, password=password, database=database)
+    cursor = connection.cursor()
+
+
+def convert_scale():
+    # convert the scale of the food data from g to the appropriate measure according to the column name in the table
+    # Database connection details
+    host = 'localhost'
+    user = 'root'
+    password = 'shachar100'
+    database = 'food_recommandation'
+
+
+    # Connect to the database
+    connection = pymysql.connect(host=host, user=user, password=password, database=database)
+    cursor = connection.cursor()
+
+
+    # Define a dictionary for the conversion scale based on column names
+    conversion_factors = {
+        'Caloric_Value_kcal' : 0.239006, # KJ to kcal
+        'Cholesterol_mg': 1000,  # g to mg
+        'Calcium_mg': 1000,       # g to mg
+        'Magnesium_mg': 1000,     # g to mg
+        'Potassium_mg': 1000,     # g to mg
+        'Vitamin_A_mg': 1000,     # µg to mg
+        'Thiamin_mg': 1000,       # µg to mg
+        'Folic_acid_mg': 1000,    # µg to mg
+        'Vitamin_B12_mg': 1000,   # µg to mg
+        'Riboflavin_mg': 1000,    # µg to mg
+        'Niacin_mg': 1000,        # µg to mg
+        'Pantothenic_acid_mg': 1000,  # µg to mg
+        'Vitamin_B6_mg': 1000,    # µg to mg
+        'Vitamin_C_mg': 1000,     # µg to mg
+        'Vitamin_D_mg': 1000,     # µg to mg
+        'Vitamin_E_mg': 1000,     # µg to mg
+        'Vitamin_K_mg': 1000,     # µg to mg
+        'Copper_mg': 1000,        # µg to mg
+        'Iron_mg': 1000,          # µg to mg
+        'Manganese_mg': 1000,     # µg to mg
+        'Phosphorus_mg': 1000,    # µg to mg
+        'Selenium_mg': 1000,      # µg to mg
+        'Zinc_mg': 1000           # µg to mg
+    }
+
+    # Loop over the columns and apply the conversion factor where applicable
+    for column, factor in conversion_factors.items():
+        # Update the table with converted values (multiply by the conversion factor)
+        update_query = f"""
+        UPDATE food
+        SET {column} = {column} * {factor}
+        """
+        cursor.execute(update_query)
+
+    # Commit the changes to the database
     connection.commit()
-    print("Data inserted successfully.")
-except Exception as e:
-    print("An error occurred:", e)
-    connection.rollback()  # Rollback in case of error
-finally:
-    # Close the cursor and connection
+
+    # Close the database connection
     cursor.close()
     connection.close()
+
+    print("Data scale conversion completed successfully.")
+
+# main
+if __name__ == '__main__':
+    load_food_data_big()
+    convert_scale()
