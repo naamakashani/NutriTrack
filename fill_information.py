8974df8499c3851195af029c07c00b3d38fd7886
naamakashani <naama.kashani@live.biu.ac.il>
2024-11-27 18:03:53 +0200

    merge to one file

--- fill_information.py
+++ fill_information.py
@@ -1,18 +1,68 @@
-import pandas as pd
 import pymysql
+import pandas as pd
+def connect_to_db():
+    # Database connection details
+    host = 'localhost'
+    user = 'root'
+    password = 'Nn021099!'
+    database = 'food_recommandation'
+    # Connect to the database
+    connection = pymysql.connect(host=host, user=user, password=password, database=database)
+    cursor = connection.cursor()
+    return connection, cursor
 
-# Database connection details
-host = 'localhost'
-user = 'root'
-password = 'Nn021099!'
-database = 'food_recommandation'
 
-# Connect to the database
-connection = pymysql.connect(host=host, user=user, password=password, database=database)
-cursor = connection.cursor()
+def life_stage_group_load(connection, cursor):
+    try:
+        csv_file_path = r'C:\Users\kashann\PycharmProjects\NutriTrack\life_stage_group.csv'
+        df_life_stage = pd.read_csv(csv_file_path)
+        df_life_stage['min_age'] = df_life_stage['min_age'].astype(int)
+        df_life_stage['max_age'] = df_life_stage['max_age'].astype(int)
+
+        # Convert vitamin columns to DECIMAL(4,4), i.e., round to 4 decimal places
+        vitamin_columns = [
+            'Vitamin_A_mg', 'Vitamin_C_mg', 'Vitamin_D_mg', 'Vitamin_E_mg', 'Vitamin_K_mg',
+            'Thiamin_mg', 'Riboflavin_mg', 'Niacin_mg', 'Vitamin_B6_mg', 'Vitamin_B12_mg', 'Pantothenic_acid_mg'
+        ]
+
+        for column in vitamin_columns:
+            df_life_stage[column] = df_life_stage[column].apply(lambda x: round(x, 4))
+
+        insert_query = """
+            INSERT INTO life_stage_group_daily_recommand 
+            (gender, subgroup, min_age, max_age, Vitamin_A_mg, Vitamin_C_mg, Vitamin_D_mg, Vitamin_E_mg, Vitamin_K_mg, 
+            Thiamin_mg, Riboflavin_mg, Niacin_mg, Vitamin_B6_mg, Vitamin_B12_mg, Pantothenic_acid_mg) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        for index, row in df_life_stage.iterrows():
+            cursor.execute(insert_query, (row['gender'],
+                                          row['subgroup'], row['min_age'], row['max_age'], row['Vitamin_A_mg'],
+                                          row['Vitamin_C_mg'], row['Vitamin_D_mg'],
+                                          row['Vitamin_E_mg'], row['Vitamin_K_mg'], row['Thiamin_mg'],
+                                          row['Riboflavin_mg'], row['Niacin_mg'],
+                                          row['Vitamin_B6_mg'], row['Vitamin_B12_mg'], row['Pantothenic_acid_mg']
+                                          ))
+        # Commit the changes
+        connection.commit()
+        print("Data inserted successfully.")
+    except Exception as e:
+        print("An error occurred:", e)
+        connection.rollback()  # Rollback in case of error
+
+def insert_teams(connection, cursor):
+    groups = ['Microsoft', 'Google', 'Amazon', 'Facebook', 'Apple', 'AngelmanFamily', 'BacharFamily', 'CohenFamily']
+    # Prepare the SQL query with placeholders
+    insert_query = "INSERT INTO team (team_name) VALUES (%s)"
+    try:
+        # Use executemany to insert multiple rows efficiently
+        cursor.executemany(insert_query, [(group,) for group in groups])
+        connection.commit()
+        print(f"{len(groups)} groups inserted successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
-def create_users():
+def create_users(connection, cursor):
     data = [
         [100000001, 'Male', 0.8, 'Infants', 'Alice_M', 7.2, 68, 'Sedentary'],
         [100000002, 'Female', 4, 'Children', 'Sophia_F', 18, 105, 'Lightly active'],
@@ -92,4 +142,31 @@ def create_users():
     print("Users created successfully")
 
 
-create_users()
+
+
+def insert_belong_teams(connection, cursor):
+    groups = [
+        (1, 100000001), (1, 100000002), (1, 100000003), (2, 100000004), (2, 100000006),
+        (3, 100000007)
+    ]
+    # Prepare the SQL query with placeholders
+    insert_query = "INSERT INTO belong_team (team_id, user_id) VALUES (%s, %s)"
+    # Iterate over the groups and execute the insert query
+    for team_id, user_id in groups:
+        cursor.execute(insert_query, (team_id, user_id))
+        connection.commit()
+
+    print("Users assigned to groups successfully")
+
+def fill_information():
+    connection, cursor = connect_to_db()
+    life_stage_group_load(connection, cursor)
+    create_users(connection, cursor)
+    insert_teams(connection, cursor)
+    insert_belong_teams(connection, cursor)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    fill_information()
+
